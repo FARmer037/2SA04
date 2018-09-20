2e8b37295a4398a826c255830d0dc1e90206f16e
staff <mustofa0367@gmail.com>
2018-09-20 12:42:30 +0700

    image

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import Weather from './components/Weather'
+
 export default class App extends React.Component {
   render() {
     return (
-      <Weather zipCode="90110"/>
+      <Weather zipCode="90110" />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
